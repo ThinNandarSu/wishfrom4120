500a5118a8c458b788a809d25459ab3117e5f5f1
Thin Thin <thinthinsu123@gmail.com>
2021-08-16 03:22:30 +0630

    Add Wish Card

--- js/app.js
+++ js/app.js
@@ -1,13 +1,3 @@
-// function play() {
-//     var audio = document.getElementById("audio");
-//     audio.play();
-// }
-
-// function turnlight() {
-//     var body = document.body;
-//     body.classList.add("light");
-// }
-
 const month = document.querySelector('.month-name');
 const date = document.querySelector('.number');
 
@@ -25,30 +15,15 @@ const getbutterfly = document.querySelector('.butterfly img');
 const getwishmesg = document.querySelector('.wishmesg');
 const getshinemessage = document.querySelector('.btn-shine');
 const gethappy30 = document.querySelector('.hbwrapper');
+const chgimg = document.querySelector('.swing img');
+const getcardbtn = document.querySelector('.heart-btn-card');
+const getwishcard = document.querySelector('.card');
+
+
 // console.log(document.querySelector('.swing img'));
 
-
-
-const chgimg = document.querySelector('.swing img');
-
-
-// Typing Text
-var text = "Sayar ...Today is your special day! I've prepared this tiny little thing for you ... ENJOY IT";
-
-var speed = 100;
-var index = 1;
-
-function autotext() {
-  gettypedtext.innerHTML = text.slice(0, index);
-
-  index++;
-  setTimeout(autotext, speed);
-}
-
-
-
 const monthNames = ["January", "February", "March", "April", "May", "June",
-  "July", "August", "September", "October", "November", "December"
+    "July", "August", "September", "October", "November", "December"
 ]
 
 const dateObj = new Date()
@@ -59,140 +34,145 @@ const monthDate = dateObj.getDate();
 month.innerHTML = monthName;
 date.innerHTML = monthDate;
 
-setTimeout(function () {
-  getcalendar.style.display = 'none';
-}, 2000);
+setTimeout(function() {
+    getcalendar.style.display = 'none';
+}, 3000);
 
 
-setTimeout(function () {
-  getballoon.style.display = 'block';
-}, 2000);
-
-setTimeout(function () {
-  gettypedtext.style.display = 'block';
-  autotext();
-}, 2500);
+setTimeout(function() {
+    getballoon.style.display = 'block';
+}, 3000);
 
 
-setTimeout(function () {
-  getballoon.remove();
-  gettypedtext.remove();
+// Typing Text
+var text = "Sayar ...Today is your special day! I've prepared this tiny little thing for you ... ENJOY IT";
+
+var speed = 100;
+var index = 1;
+
+function autotext() {
+    gettypedtext.innerHTML = text.slice(0, index);
+
+    index++;
+    setTimeout(autotext, speed);
+}
+
+setTimeout(function() {
+    gettypedtext.style.display = 'block';
+    autotext();
+}, 3000);
+
+setTimeout(function() {
+    getballoon.remove();
+    gettypedtext.remove();
 }, 14000);
 
-setTimeout(function () {
-  getcountdown.style.display = 'block';
+setTimeout(function() {
+    getcountdown.style.display = 'block';
 }, 14000);
 
-setTimeout(function () {
-  getcountdown.remove();
+setTimeout(function() {
+    getcountdown.remove();
 }, 18000);
 
-setTimeout(function () {
-  getballoon.remove();
-  gettypedtext.remove();
-  getcountdown.remove();
+setTimeout(function() {
+    getballoon.remove();
+    gettypedtext.remove();
+    getcountdown.remove();
 }, 19000);
 
-
-setTimeout(function () {
-  getheartbtn.style.display = 'block';
+setTimeout(function() {
+    getheartbtn.style.display = 'block';
 }, 19000);
 
 getheartbtn.addEventListener('click', () => {
-  playaudio();
+    playaudio();
 
-  getbanner.style.display = 'block';
+    getbanner.style.display = 'block';
+
+    setTimeout(function() {
+        getheartbtn.remove();
+        getstreamer.style.display = 'block';
+        getcat.style.display = 'block';
+
+    }, 2000);
+
+
+    setTimeout(function() {
+        // getstreamer.style.display = 'none';
+        getbanner.remove;
+    }, 9000);
+
+    setTimeout(function() {
+        getstreamer.style.display = 'none';
+        getswingpic.style.display = 'block';
+        getbutterfly.style.display = 'block';
+    }, 10000);
+
+    // setTimeout(function() {
+    //     getcake.style.display = 'block';
+    // }, 15000);
+
+    setTimeout(function() {
+        getcake.style.display = 'block';
+    }, 15000);
+
+    setTimeout(function() {
+        getcake.style.display = 'none';
+        getcat.style.display = 'none';
+        getbanner.style.display = 'none';
+    }, 18000);
+
+
+    setTimeout(function() {
+        getwishmesg.style.display = 'block';
+        wish();
+    }, 18000);
+
+
+    var wishmesg = "Happy Birthday To A Great Software Developer, my beloved teacher " +
+        "who has the most beautiful soul, I feel very grateful and fortunate to say that I have been a student of yours!";
+    var wishspeed = 150;
+    var idx = 1;
+
+    function wish() {
+        getwishmesg.innerHTML = wishmesg.slice(0, idx);
+
+        idx++;
+        setTimeout(wish, wishspeed);
+    }
+
+    setTimeout(function() {
+        getwishmesg.remove();
+        getbutterfly.remove();
+        getshinemessage.style.display = 'block';
+
+        chgimg.src = "img/sayar_1.jpg";
+
+    }, 48000);
+
+    setTimeout(function() {
+        getshinemessage.remove();
+        gethappy30.style.display = 'block';
+    }, 55000);
+
+    setTimeout(function() {
+        getshinemessage.remove();
+        gethappy30.style.display = 'block';
+        getcardbtn.style.display = 'block';
+        chgimg.src = "img/sayar_2.jpg";
+    }, 55000);
+
+    getcardbtn.addEventListener('click', () => {
+        getwishcard.style.display = 'block';
+        gethappy30.style.display = 'none';
+        getcardbtn.style.display = 'none';
+        getwishcard.style.display = 'block';
+    });
 
 });
 
 function playaudio() {
-  var audio = document.getElementById("audio");
-  audio.play();
+    var audio = document.getElementById("audio");
+    audio.play();
 }
-
-
-setTimeout(function () {
-  getheartbtn.remove();
-  getstreamer.style.display = 'block';
-  getcat.style.display = 'block';
-
-}, 25000);
-
-
-setTimeout(function () {
-  getstreamer.style.display = 'none';
-  getbanner.remove;
-}, 30000);
-
-setTimeout(function () {
-  getswingpic.style.display = 'block';
-  getbutterfly.style.display = 'block';
-}, 30000);
-
-setTimeout(function () {
-  getcake.style.display = 'block';
-}, 35000);
-
-setTimeout(function () {
-  getcake.style.display = 'block';
-}, 35000);
-
-setTimeout(function () {
-  getcake.style.display = 'none';
-  getcat.style.display = 'none';
-  getbanner.style.display = 'none';
-}, 40000);
-
-
-setTimeout(function () {
-  getwishmesg.style.display = 'block';
-  wish();
-}, 40000);
-
-
-var wishmesg = "Happy Birthday To A Great Software Developer, my beloved teacher " +
-  "who has the most beautiful soul, I feel very grateful and fortunate to say that I have been a student of yours!";
-var wishspeed = 150;
-var idx = 1;
-function wish() {
-  getwishmesg.innerHTML = wishmesg.slice(0, idx);
-
-  idx++;
-  setTimeout(wish, wishspeed);
-}
-
-setTimeout(function () {
-  getwishmesg.remove();
-  getbutterfly.remove();
-  getshinemessage.style.display = 'block';
-
-  chgimg.src = "img/sayar_1.jpg";
-
-}, 70000);
-
-setTimeout(function () {
-  getshinemessage.remove();
-  gethappy30.style.display = 'block';
-}, 76000);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
